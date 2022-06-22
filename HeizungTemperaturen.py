5761a17ed9cd4932fc347dacb0af497685662b30
bene <skurz@hit-john.de>
2022-06-22 16:41:29 +0200

    Anzahl derZugriffe auf Webseite reduziert

--- HeizungTemperaturen.py
+++ HeizungTemperaturen.py
@@ -1,32 +1,25 @@
 # Ermitteln der Heizungstemperaturen und der Aussentemperatur
-# Ausgabe an sqlite3
+# sk,22,06,22 Ausgabe an sqlite3
+# sk,22,06,22 Anzahl derZugriffe auf Webseite reduziert
+
 from multiprocessing.connection import wait
 from time import sleep
 import requests, os, datetime, time
 from bs4 import BeautifulSoup
 import sqlite3
-MEASUREMENT_INTERVAL=20
+MEASUREMENT_INTERVAL=10
 DB_NAME='Temperaturen.db'
 TABLE_NAME='Temperaturen'
+VERBOSE=False
 
+BASE_URL='http://www.wetterwarte-sued.com/v_1_0/aktuelles/messwerte/messwerte_aktuell_ochsenhausenstadt.php'
+WEATHER_STATIONS=['weatherstation_29','weatherstation_69']
 
+def get_info_from_station(soup, station_id):
 
-OX_Stadt='http://www.wetterwarte-sued.com/v_1_0/aktuelles/messwerte/messwerte_aktuell_ochsenhausenstadt.php'
-
-station_dict= {
-	'OX_Stadt' : ['http://www.wetterwarte-sued.com/v_1_0/aktuelles/messwerte/messwerte_aktuell_ochsenhausenstadt.php','weatherstation_69'],
-	'Erlenmoos' : ['http://www.wetterwarte-sued.com/v_1_0/aktuelles/messwerte/messwerte_aktuell_erlenmoos.php', 'weatherstation_29']
-}
-
-def get_temp_from_site(station_entry):
-	site = station_entry[0]
-	station_id = station_entry[1]
-	temperaturecurrent = None
-	station_name = None
-	r = requests.get(site)
-	soup = BeautifulSoup(r.text, 'html.parser')
 	links = soup.findAll('tr', id=station_id)
-
+	station_name = ''
+	temperaturecurrent = -200
 	for link in links:
 		table_data = link.find_all('td')	
 		for table_entry in table_data:
@@ -37,15 +30,11 @@ def get_temp_from_site(station_entry):
 				#return temperaturecurrent
 			if 'name' in str_table_entry:
 				station_name = table_entry.text
-			
-
 	return temperaturecurrent, station_name
-
 
 if not os.path.isfile(DB_NAME):
 	conn = sqlite3.connect(DB_NAME)
 	cur = conn.cursor()
-	# cur.execute('create table ' + TABLE_NAME + '(UnixTime integer, ts timestamp, ATemp)')
 	cur.execute('create table ' + TABLE_NAME + '(UnixTime integer, ATemp, VTemp, RTemp)')
 else:
 	conn = sqlite3.connect(DB_NAME)
@@ -53,29 +42,31 @@ else:
 
 i = 0 
 while i < 20:
-	no_of_stations = len(station_dict.keys())
-	print(str(no_of_stations))
+	r = requests.get(BASE_URL)
+	soup = BeautifulSoup(r.text, 'html.parser')
 	temp_arr = []
-	for key in station_dict.keys():
-		temp, station_name = get_temp_from_site(station_dict.get(key))
+	no_of_stations = len(WEATHER_STATIONS)
+	for station_id in WEATHER_STATIONS:
+		temp, station_name = get_info_from_station(soup, station_id)
 		float_temp = float(temp.replace(',','.'))
 		temp_arr.append(float_temp)
-		print('Die Temperatur der Station ' + station_name + ' ist: ' + temp)
+		if VERBOSE: 
+			print('Die Temperatur der Station ' + station_name + ' ist: ' + temp)
 
 	now = datetime.datetime.now()
 	unixtime = time.mktime(now.timetuple())
 	
 	ATemp = round(sum(temp_arr)/no_of_stations,5)
+	#TODO
 	VTemp = float(ATemp + 30.0)
 	RTemp = float(ATemp + 25.0)
 
-	print('Mittelwert: ' + str(ATemp))
+	if VERBOSE: 
+		print('Mittelwert: ' + str(ATemp))
 	with conn:
 		cur.execute('insert into ' + TABLE_NAME + '(UnixTime, ATemp, VTemp, RTemp) values (?, ?, ?, ?)', (unixtime, ATemp, VTemp, RTemp))
-		#conn.commit()
 	sleep(MEASUREMENT_INTERVAL)
 	i = i + 1
 conn.close()
-print('Feddisch')
-
-
+if VERBOSE: 
+	print('Feddisch')
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Ermitteln der Heizungstemperaturen und der Aussentemperatur:
 
 -   [x] Ermitteln der Aussentemperatur über Webscraping eines Webservice
 -   [x] Speichern der Daten mit SQLite3
--   [x] Auswerten der Daten mit Grafana funktioniert
+-   [x] Auswerten der Daten mit Grafana
 -   [ ] Ermitteln der Heizugstemperaturen mit dem DS18B20-Sensor
