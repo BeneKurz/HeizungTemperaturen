7abbddaeda49fb39c18ad9aa5a59445de5c69d50
bene <skurz@hit-john.de>
2022-07-04 16:34:47 +0200

    Variablen ausgelagert in config.cfg

--- show_temp.py
+++ show_temp.py
@@ -2,21 +2,47 @@
 show_temp.py
 Temperaturen anzeigen mit dash
 sk,04,07,22 Erster Versuch
+sk,04,07,22 Variablen ausgelagert in config.cfg
 '''
 from datetime import date
 import pandas as pd
 import dash
 from dash import dash_table
-import sqlite3
+import sqlite3, os
 from dash import Dash, html, dcc
 import plotly.express as px
 from dateutil import tz
 from dash.dependencies import Input, Output
 
+CFG_FILE='.\config.cfg'
+# MEASUREMENT_INTERVAL_SECONDS=60
+# WEATHER_STATION_ACCESS_INTERVAL_SECONDS=60*15
+# INVALID_TEMP_STR='-30.0'
 
-conn = sqlite3.connect('Temperaturen.db')
+if os.path.isfile(CFG_FILE):       
+	s = open(CFG_FILE, 'r').read() 
+	SENSORS = {}                  
+	try:                          
+		GLOBALS = eval(s)
+		MEASUREMENT_INTERVAL_SECONDS=GLOBALS.get('MEASUREMENT_INTERVAL_SECONDS')
+		WEATHER_STATION_ACCESS_INTERVAL_SECONDS=GLOBALS.get('WEATHER_STATION_ACCESS_INTERVAL_SECONDS')
+		INVALID_TEMP_STR=GLOBALS.get('INVALID_TEMP_STR')
+		DB_NAME=GLOBALS.get('DB_NAME')
+		TABLE_NAME=GLOBALS.get('TABLE_NAME')
+		VERBOSE= GLOBALS.get('VERBOSE')
+		SENSORS = GLOBALS.get('SENSORS')
+	except:     
+		print('error config file '+ CFG_FILE)  
+
+if (os.name == 'nt'):                                            
+    DB_FILENAME=DB_NAME
+else:                                                            
+	DB_FILENAME='/var/lib/grafana/' + DB_NAME
+
+
+conn = sqlite3.connect(DB_FILENAME)
 #cursor =conn.cursor()
-sel_string = 'SELECT * FROM temperaturen'
+sel_string = 'SELECT * FROM ' + TABLE_NAME
 #cursor.execute(sel_string)
 
 df = pd.read_sql(sel_string,conn)
@@ -27,17 +53,14 @@ app = dash.Dash(__name__)
 df['UnixTime'] = pd.to_datetime(df['UnixTime'],unit='s', utc=True).apply(lambda x: x.tz_convert('Europe/Berlin'))
 Temps = px.line(df, x=df['UnixTime'], y=df.columns)
 
-# @app.callback(Output("histogramm", "figure"),[Input("verteilung-dropdown", "value"), Input("n-input", "value")])
 app.layout = html.Div([
-    # html.Div(dcc.Input(id='input-box', type='text')),
-    # html.Button('Submit', id='button-example-1'),
-    # html.Div(id='output-container-button', children='Enter a value and press submit'),
     # dcc.Dropdown(['New York City', 'Montreal', 'San Francisco'], 'Montreal'),
 
     dcc.Markdown('''
         #### Dash and Markdown
         Dash supports [Markdown](http://commonmark.org/help).
         Markdown is a simple way to write and format text.
+
         It includes a syntax for things like **bold text** and *italics*,
         [links](http://commonmark.org/help), inline `code` snippets, lists,
         quotes, and more.
